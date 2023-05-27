58765aaa7bbd6bb24010660261f1f2fa5af13eed
Jaymoo87 <114165259+Jaymoo87@users.noreply.github.com>
2023-05-27 18:03:38 -0500

    dalle ui started... might just stick with gpt styling

--- src/client/pages/DALLE.tsx
+++ src/client/pages/DALLE.tsx
@@ -3,7 +3,26 @@ import React from 'react';
 type Props = {};
 
 const DALLE = (props: Props) => {
-  return <div>DALLE</div>;
+  const surpriseOptions = [
+    'A Blue Ostrich eating melon',
+    'A matisse style shark on the telephone',
+    'the inevitable end of an era',
+  ];
+
+  return (
+    <div className="dalle-app">
+      <section className="search-section">
+        <p>
+          Start with detailed description <span className="surprise">surprise me</span>
+        </p>
+        <div className="input-container">
+          <input placeholder="a surprising image with some flair" />
+          <button>Generate</button>
+        </div>
+      </section>
+      <section className="image-section"></section>
+    </div>
+  );
 };
 
 export default DALLE;
